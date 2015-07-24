62c8eac2189e2345b0061db08ae7bdaf2775fc45
Redpoint1 <Redpoints1@gmail.com>
2015-07-25 01:42:00 +0200

    Added ngStorage, Language class, rendering languages

--- assets/js/clickrammer.js
+++ assets/js/clickrammer.js
@@ -7,7 +7,32 @@
 "use strict";
 
 (function() {
-  var clickrammer = angular.module("game", []);
+
+  function Language(data){
+    this._set_data(data);
+  }
+
+  Language.prototype = {
+    id          : null ,
+    name        : null ,
+    logo        : null ,
+    description : null ,
+    requirements: []   ,
+    timeToLearn : 0    ,
+    learned     : false,
+
+    _set_data: function(data){
+      for(var property in data){
+        if(typeof this[property] != "undefined"){
+          this[property] = data[property];
+        }
+      }
+    }
+  }
+
+  //AngularJS part
+
+  var clickrammer = angular.module("game", ['ngStorage']);
 
   clickrammer.controller("MainCtrl", ['$scope', function($scope) {
     $scope.page = 'templates/projects.html'; //default
@@ -15,6 +40,14 @@
     $scope.setPage = function(page) {
       $scope.page = 'templates/'+page+'.html';
     }
+
+  }]);
+
+  clickrammer.controller("LanguageCtrl", ['$scope', function($scope) {
+    $scope.languages = [
+      new Language(languages[0]),
+      new Language(languages[1])
+    ]
 
   }]);
 
---
+++ assets/js/init.js
@@ -0,0 +1,18 @@
+var languages = [
+  {
+    id: "html",
+    name: "HTML",
+    logo: 'html.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio ex neque laudantium fugit, nemo quo magnam eaque sunt, expedita asperiores sapiente veritatis enim aliquid, vero corporis. Animi, tenetur dolorem eaque.',
+    requirements: [],
+    timeToLearn: 0,
+  },
+  {
+    id: "js",
+    name: "Javascript",
+    logo: 'js.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio ex neque laudantium fugit, nemo quo magnam eaque sunt, expedita asperiores sapiente veritatis enim aliquid, vero corporis. Animi, tenetur dolorem eaque.',
+    requirements: ["html"],
+    timeToLearn: 120,
+  },
+]
