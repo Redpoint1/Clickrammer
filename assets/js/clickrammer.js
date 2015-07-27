e31a824eeeadc163a834ae99b838602e0c19797d
Redpoint1 <Redpoints1@gmail.com>
2015-07-27 21:58:49 +0200

    Add User(), moved Language() into User(), LEARN button has been showed only if you have all requirements

--- assets/js/clickrammer.js
+++ assets/js/clickrammer.js
@@ -8,49 +8,81 @@
 
 (function() {
 
-  function Language(data){
-    this._set_data(data);
-  }
+  function makeArray(data){
+    var array = [];
 
-  Language.prototype = {
-    id          : null ,
-    name        : null ,
-    logo        : null ,
-    description : null ,
-    requirements: []   ,
-    timeToLearn : 0    ,
-    learned     : false,
-
-    _set_data: function(data){
-      for(var property in data){
-        if(typeof this[property] != "undefined"){
-          this[property] = data[property];
-        }
-      }
+    for(var i=0; i<data.length; i++){
+      array.push(data[i]);
     }
+    return array;
   }
 
   //AngularJS part
 
   var clickrammer = angular.module("game", ['ngStorage']);
 
-  clickrammer.factory("User", ['$localStorage', function($localStorage){
-    function User(){
-      this.load();
+  clickrammer.factory("Language", function(){
+    function Language(data){
+      this._set_data(data);
+    }
+
+    Language.prototype = {
+      id          : null ,
+      name        : null ,
+      logo        : null ,
+      description : null ,
+      requirements: []   ,
+      timeToLearn : 0    ,
+      learned     : false,
+
+      _set_data: function(data){
+        for(var property in data){
+          if(typeof this[property] != "undefined"){
+            this[property] = data[property];
+          }
+        }
+      },
+      canBeLearned: function(user){
+        for (var i=0; i<this.requirements.length; i++){
+          if (!user.data.languages[this.requirements[i]].learned){
+            return false;
+          }
+        }
+        return true;
+      }
+    }
+
+    return Language;
+  });
+
+  clickrammer.factory("User", ['$localStorage', 'Language', function($localStorage, Language){
+    function User(infos){
+      this.load(infos);
     }
 
     User.prototype = {
       data: $localStorage,
 
-      load: function(){
+      load: function(infos){
+        //this.data.$reset();
         this.data.$default({
           lines : 0,
-          languages: []
+          languages: {}
         });
-      },
 
+        this._load_languages_info(infos.languages);
+      },
       click: function(){
         this.data.lines++;
+      },
+      _load_languages_info: function(languages){
+        for (var i=0; i<languages.length; i++){
+          var lang = languages[i];
+          this.data.languages[lang.id] = new Language(lang);
+          if(typeof this.data.languages[lang.id] != "undefined"){
+            this.data.languages[lang.id]._set_data(lang);
+          }
+        }
       }
     }
 
@@ -68,7 +100,7 @@
 
       start: function(){
         this.data.$default({
-          languages: [for (lang of _languages) new Language(lang)]
+          languages: makeArray(_languages),
         });
       }
     }
@@ -78,17 +110,13 @@
 
   clickrammer.controller("MainCtrl", ['$scope', 'Game', 'User', function($scope, Game, User) {
 
-    $scope.user = new User();
     $scope.game = new Game();
+    $scope.user = new User($scope.game.data);
 
-    $scope.page = 'templates/projects.html'; //default
+    $scope.page = 'templates/languages.html'; //default
     $scope.setPage = function(page) {
       $scope.page = 'templates/'+page+'.html';
     }
-
-  }]);
-
-  clickrammer.controller("LanguageCtrl", ['$scope', function($scope) {
 
   }]);
 
